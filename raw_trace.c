4fe59aeb6b1634bb39d8943c1ae9684a93908fbf
Oguzhan Ince <oguzhan.ince@protonmail.com>
2023-01-04 19:27:59 +0300

    mac

--- raw_trace.c
+++ raw_trace.c
@@ -1,153 +1,183 @@
-#include <stdio.h>				// lib for printf()  and fprintf()
-#include <stdlib.h> 			// lib for rand()  and exit()
-#include <unistd.h> 			// lib for getpid()  and close()
-#include <string.h>				// lib for memset() and memcpy() 
-#include <arpa/inet.h>		// lib for inet_addr() and inet_ntoa() 
+#include <stdio.h>		 // lib for printf()  and fprintf()
+#include <stdlib.h>		 // lib for rand()  and exit()
+#include <unistd.h>		 // lib for getpid()  and close()
+#include <string.h>		 // lib for memset() and memcpy()
+#include <arpa/inet.h> // lib for inet_addr() and inet_ntoa()
 
-#define __USE_BSD					/* use bsd'ish ip header */
+#define __USE_BSD /* use bsd'ish ip header */
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netinet/ip.h> 
-#define __FAVOR_BSD				/* use bsd'ish tcp header */
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+
+#define __FAVOR_BSD /* use bsd'ish tcp header */
 #include <netinet/tcp.h>
 #include <errno.h>
 #include <time.h>
 
-#include <sysexits.h> 		// lib for EX_USAGE and EX_OSERR	
-#include <err.h>					// lib for err() and errx()
+#include <sysexits.h> // lib for EX_USAGE and EX_OSERR
+#include <err.h>			// lib for err() and errx()
 
 #define P 34555
 
-unsigned short csum (unsigned short *buf, int nwords)
+
+// dumps raw memory in hex byte and printable split format
+void dump(const unsigned char *data_buffer, const unsigned int length)
 {
-  unsigned long sum;
-  for (sum = 0; nwords > 0; nwords--)
-    sum += *buf++;
-  sum = (sum >> 16) + (sum & 0xffff);
-  sum += (sum >> 16);
-  return ~sum;
+	unsigned char byte;
+	unsigned int i, j;
+	for(i=0; i < length; i++) {
+		byte = data_buffer[i];
+		fprintf(stdout, "%02x ", data_buffer[i]);  // display byte in hex
+		if(((i%16)==15) || (i==length-1)) {
+			for(j=0; j < 15-(i%16); j++)
+				fprintf(stdout, "   ");
+			fprintf(stdout, "| ");
+			for(j=(i-(i%16)); j <= i; j++) {  // display printable bytes from line
+				byte = data_buffer[j];
+				if((byte > 31) && (byte < 127)) // outside printable char range
+					fprintf(stdout, "%c", byte);
+				else
+					fprintf(stdout, ".");
+			}
+			fprintf(stdout, "\n"); // end of the dump line (each line 16 bytes)
+		} // end if
+	} // end for
 }
 
 
-void delay(int number_of_seconds) 
-{ 
-    // Approximating to meet enough delay, so that my icmp listener has enough time to go to I/O burst and get back to listening
-    int approx_time = 10000 * number_of_seconds; 
-  
-    // Stroing start time 
-    clock_t start_time = clock(); 
-  
-    // looping till required time is not acheived 
-    while (clock() < start_time + approx_time) 
-    	printf("");//"%d\n",clock());
-        ; 
+
+// generic checksum calculation algorithm
+unsigned short cksum(unsigned short *addr, int len)
+{
+	int nleft = len;
+	int sum = 0;
+	unsigned short *w = addr;
+	unsigned short answer = 0;
+
+	while (nleft > 1)
+	{
+		sum += *w++;
+		nleft -= 2;
+	}
+
+	if (nleft == 1)
+	{
+		*(unsigned char *)(&answer) = *(unsigned char *)w;
+		sum += answer;
+	}
+
+	sum = (sum >> 16) + (sum & 0xffff);
+	sum += (sum >> 16);
+	answer = ~sum;
+
+	return (answer);
 }
+
+
+
+void delay(int number_of_seconds)
+{
+	// Approximating to meet enough delay, so that my icmp listener has enough time to go to I/O burst and get back to listening
+	int approx_time = 10000 * number_of_seconds;
+
+	// Stroing start time
+	clock_t start_time = clock();
+
+	// looping till required time is not acheived
+	while (clock() < start_time + approx_time)
+		printf(""); //"%d\n",clock());
+	;
+}
+
 
 
 int main(int argc, char *argv[])
 {
-		char IPADDRESS[13];
-		char datagram[4096];
+	/* this buffer will contain ip header, tcp header,
+		 and payload. we'll point an ip header structure
+		 at its beginning, and a tcp header structure after
+		 that to write the header values into it */
 
-			/* this buffer will contain ip header, tcp header,
-			   and payload. we'll point an ip header structure
-			   at its beginning, and a tcp header structure after
-			   that to write the header values into it */
+	int transmit_s, receive_s, rc;
+	struct protoent *p;
+	struct sockaddr_in sin;
+	struct ip ip;
+	struct icmp icmp;
 
-    if (argc != 2)
-        errx(EX_USAGE, "Usage: %s <IP address>", argv[0]);
+	if (argc != 2)
+		errx(EX_USAGE, "Usage: %s <IP address>", argv[0]);
 
-    /* open raw socket */
-		int s = socket (PF_INET, SOCK_RAW, IPPROTO_TCP);		
+	memset(&sin, 0, sizeof(sin));
+	sin.sin_family = PF_INET;
+	sin.sin_port = 0;
 
-    if (s < 0)
-        err(EX_OSERR, "s raw socket");
+	/* Parse command line address. */
+	if (inet_pton(PF_INET, argv[1], &sin.sin_addr) <= 0)
+		err(EX_USAGE, "Parse address");
 
+	/* open raw socket */
+	transmit_s = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
+	if (transmit_s < 0)
+		err(EX_OSERR, "transmit_s raw socket");
 
-		struct ip *iph = (struct ip *) datagram;
-		struct tcphdr *tcph = (struct tcphdr *) datagram + sizeof (struct ip);
-		struct sockaddr_in sin;
+	for (int i = 1; i < 20; i++)
+	{
+		delay((i * 2) + 1); // wait for (i*2)+1 hops, then send out next packet
+		
+		/* Fill in the IP header. */
+		memset(&ip, 0x0, sizeof(ip));
+		ip.ip_v = IPVERSION;
+		ip.ip_hl = sizeof(ip) >> 2;
+		ip.ip_tos = 0;
+		ip.ip_len = sizeof(ip) + sizeof(icmp);
+		ip.ip_id = htons(getpid());
+		ip.ip_off = 0;
+		ip.ip_ttl = i;
+		ip.ip_p = IPPROTO_RAW;
+		ip.ip_sum = cksum((unsigned short *)&ip, sizeof(ip));
+		ip.ip_src.s_addr = inet_addr("10.28.28.28");
+		ip.ip_dst.s_addr = inet_addr(argv[1]);
 
-		sin.sin_family = AF_INET;
-		sin.sin_port = htons(P);
+		/* Fill in the ICMP header. */
+		memset(&icmp, 0x0, sizeof(icmp));
+		icmp.icmp_type = ICMP_ECHO;
+		icmp.icmp_code = 0;
+		icmp.icmp_id = getpid();
+		icmp.icmp_seq = 1;
+		icmp.icmp_cksum = cksum((unsigned short *)&icmp, sizeof(icmp));
 
-				/* you byte-order >1byte header values to network
-			     byte order (not needed on big endian machines) */
+		/* finally, it is very advisable to do a IP_HDRINCL call, to make sure
+			 that the kernel knows the header is included in the data, and doesn't
+			 insert its own header into the packet before our data;
+			 got this off the internet */
 
-		/* Parse command line address. */
-    if (inet_pton(AF_INET, argv[1], &sin.sin_addr) <= 0)
-        err(EX_USAGE, "Parse address");
+		/*{  
+			int one = 1;
+			const int *val = &one;
+			// IPPROTO_RAW is 255 unknown protocol and dont get a response
+			if (setsockopt(transmit_s, IPPROTO_IP, IP_HDRINCL, val, sizeof(one)) < 0)
+				printf("Warning: Cannot set HDRINCL!\n");
+		} */
 
-		// printf("Enter host address\n");
-  	// scanf("%s", IPADDRESS);
-	  // sin.sin_addr.s_addr = inet_addr (IPADDRESS);
-
-		for (int i = 1; i < 30; i++)
+		/* Send it off. */
+		rc = sendto(transmit_s, &icmp, sizeof(icmp), 0, (struct sockaddr *)&sin, sizeof(sin));
+		if (rc < 0) { err(EX_OSERR, "error sendto sendlen=%d error no = %d\n", rc, errno); }
+		else 
 		{
-				delay((i * 2) + 1); // wait for (i*2)+1 hops, then send out next packet
-				/* we'll now fill in the ip/tcp header values, see above for explanations */
-				iph->ip_hl = 5;
-				iph->ip_v = 4;
-				iph->ip_tos = 0;
-				iph->ip_len = sizeof(struct ip) + sizeof(struct tcphdr); /* no payload */
-				iph->ip_id = htonl(54321);															 /* the value doesn't matter here */
-				iph->ip_off = 0;
-				iph->ip_ttl = i;
-				iph->ip_p = 6;
-				iph->ip_sum = 0;																 /* set it to 0 before computing the actual checksum later */
-				iph->ip_src.s_addr = inet_addr("192.168.1.109"); /* SYN's can be blindly spoofed */
-				iph->ip_dst.s_addr = sin.sin_addr.s_addr;
-
-				tcph->th_sport = htons(1234); /* arbitrary port */
-				tcph->th_dport = htons(P);
-				tcph->th_seq = random(); 			/* in a SYN packet, the sequence is a random */
-				tcph->th_ack = 0;				 			/* number, and the ack sequence is 0 in the 1st packet */
-				tcph->th_x2 = 0;
-				tcph->th_off = 0;						 /* first and only tcp segment */
-				tcph->th_flags = TH_SYN;		 /* initial connection request */
-				tcph->th_win = htonl(65535); /* maximum allowed window size */
-				tcph->th_sum = 0;						 /* if you set a checksum to zero, your kernel's IP stack
-														should fill in the correct checksum during transmission */
-				tcph->th_urp = 0;
-
-				iph->ip_sum = csum((unsigned short *)datagram, iph->ip_len >> 1);
-
-				/* finally, it is very advisable to do a IP_HDRINCL call, to make sure
-					 that the kernel knows the header is included in the data, and doesn't
-					 insert its own header into the packet before our data;
-					 got this off the internet */
-
-				{ /* lets do it the ugly way.. */
-					int one = 1;
-					const int *val = &one;
-					/* IPPROTO_RAW is 255 unknown protocol and dont get a response */
-					if (setsockopt(s, IPPROTO_IP, IP_HDRINCL, val, sizeof(one)) < 0)
-						printf("Warning: Cannot set HDRINCL!\n");
-				}
-				int tmp = 0;
-				/* send it off. */
-				{
-					if (tmp = sendto(s,												/* our socket */
-													 datagram,								/* the buffer containing headers and data */
-													 iph->ip_len,							/* total length of our datagram */
-													 0,												/* routing flags, normally always 0 */
-													 (struct sockaddr *)&sin, /* socket addr, just like in */
-													 sizeof(sin)) == -1)
-						// sizeof (sin)) < 0)		/* a normal send() */
-						err(EX_OSERR, "error sendto sendlen=%d error no = %d\n", tmp, errno);
-					else
-						printf(".\n");
-				}
-				// close(s);
-				// fprintf(stdout, "Closed transmit_s\n\n");
-		}
-
-		/*
-    int fd1 = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
-		char buf[8192];
-		while(read(fd1, buf, 8192)>0)
-		printf("%s\n",buf);//+sizeof(struct iphdr)+sizeof(struct tcphdr));
-   */
-
-		return 0;
+			fprintf(stdout, "\n\nSENT %d BYTES\n", rc);
+			fprintf(stdout, "-------------\n");
+			fprintf(stdout, "src  IP\t\t: %s\n", inet_ntoa(ip.ip_src));
+			fprintf(stdout, "dst  IP\t\t: %s\n", inet_ntoa(ip.ip_dst));
+			fprintf(stdout, "IP ID\t\t: %d\n", ntohs(ip.ip_id));
+			fprintf(stdout, "ICMP ID\t\t: %d\n", ntohs(icmp.icmp_id));
+			fprintf(stdout, "ICMP Type\t: %d\n", icmp.icmp_type);
+			fprintf(stdout, "ICMP Code\t: %d\n", icmp.icmp_code);
+			fprintf(stdout, "Seq Number\t: %d\n", ntohl(icmp.icmp_seq));
+			fprintf(stdout, "ICMP Checksum\t: %d\n", ntohs(icmp.icmp_cksum));
+			dump((unsigned char *)&icmp, sizeof(icmp));	
+		} 
+		
+	}
+	return 0;
 }
--- raw_trace_cap.c
+++ raw_trace_cap.c
@@ -1,67 +1,67 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<netinet/ip_icmp.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/ip_icmp.h>
 
 // dumps raw memory in hex byte and printable split format
 void dump(const unsigned char *data_buffer, const unsigned int length)
 {
 	unsigned char byte;
 	unsigned int i, j;
-	for(i=0; i < length; i++) {
+	for (i = 0; i < length; i++)
+	{
 		byte = data_buffer[i];
-		fprintf(stdout, "%02x ", data_buffer[i]);  // display byte in hex
-		if(((i%16)==15) || (i==length-1)) {
-			for(j=0; j < 15-(i%16); j++)
+		fprintf(stdout, "%02x ", data_buffer[i]); // display byte in hex
+		if (((i % 16) == 15) || (i == length - 1))
+		{
+			for (j = 0; j < 15 - (i % 16); j++)
 				fprintf(stdout, "   ");
 			fprintf(stdout, "| ");
-			for(j=(i-(i%16)); j <= i; j++) {  // display printable bytes from line
+			for (j = (i - (i % 16)); j <= i; j++)
+			{ // display printable bytes from line
 				byte = data_buffer[j];
-				if((byte > 31) && (byte < 127)) // outside printable char range
+				if ((byte > 31) && (byte < 127)) // outside printable char range
 					fprintf(stdout, "%c", byte);
 				else
 					fprintf(stdout, ".");
 			}
 			fprintf(stdout, "\n"); // end of the dump line (each line 16 bytes)
-		} // end if
-	} // end for
+		}												 // end if
+	}													 // end for
 }
-
 
 int main()
 {
-		// receiver socket // PF or AF_INET doesnt matter 
-		// IPPROTO_RAW is 255 unknown protocol and dont get a response
-		int receive_s = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
-		u_char buffer[1500];
-		struct sockaddr_in sin;
-		socklen_t sinlen = sizeof(sin);		
+	// receiver socket // PF or AF_INET doesnt matter
+	// IPPROTO_RAW is 255 unknown protocol and dont get a response
+	int receive_s = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
+	u_char buffer[1500];
+	struct sockaddr_in sin;
+	socklen_t sinlen = sizeof(sin);
 
+	while (1)
+	{
+		// int ret = recv(receive_s, &buffer, sizeof(buffer), 0);
+		int ret = recvfrom(receive_s, &buffer, sizeof(buffer), 0, (struct sockaddr *)&sin, &sinlen);
 
-		while(1)
-		{
-			// int ret = recv(receive_s, &buffer, sizeof(buffer), 0);
-			int ret = recvfrom(receive_s, &buffer, sizeof(buffer), 0, (struct sockaddr *)&sin, &sinlen);
+		fprintf(stdout, "\n\nRECV %d BYTES\n", ret);
+		fprintf(stdout, "-------------\n");
+		// received packet
+		struct ip *ip_recv = (struct ip *)buffer;
+		struct icmp *icmp_recv = (struct icmp *)(buffer + (ip_recv->ip_hl << 2));
+		fprintf(stdout, "src  IP\t\t: %s\n", inet_ntoa(ip_recv->ip_src));
+		fprintf(stdout, "dst  IP\t\t: %s\n", inet_ntoa(ip_recv->ip_dst));
+		fprintf(stdout, "ICMP ID\t\t: %d\n", ntohs(icmp_recv->icmp_id));
+		fprintf(stdout, "ICMP Type\t: %d\n", icmp_recv->icmp_type);
+		fprintf(stdout, "ICMP Code\t: %d\n", icmp_recv->icmp_code);
+		fprintf(stdout, "Seq Number\t: %d\n", ntohl(icmp_recv->icmp_seq));
+		fprintf(stdout, "ICMP Checksum\t: %d\n", ntohs(icmp_recv->icmp_cksum));
+		dump((unsigned char *)&icmp_recv, ret);
+	}
 
-			fprintf(stdout, "\n\nRECV %d BYTES\n", ret);
-			fprintf(stdout, "-------------\n");	
-			// received packet
-			struct ip *ip_recv = (struct ip *)buffer;
-			struct icmp *icmp_recv = (struct icmp *)(buffer + (ip_recv->ip_hl << 2));
-			fprintf(stdout, "src  IP\t\t: %s\n", inet_ntoa(ip_recv->ip_src));
-			fprintf(stdout, "dst  IP\t\t: %s\n", inet_ntoa(ip_recv->ip_dst));
-			fprintf(stdout, "ICMP ID\t\t: %d\n", ntohs(icmp_recv->icmp_id));
-			fprintf(stdout, "ICMP Type\t: %d\n", icmp_recv->icmp_type);
-			fprintf(stdout, "ICMP Code\t: %d\n", icmp_recv->icmp_code);
-			fprintf(stdout, "Seq Number\t: %d\n", ntohl(icmp_recv->icmp_seq));
-			fprintf(stdout, "ICMP Checksum\t: %d\n", ntohs(icmp_recv->icmp_cksum));
-			dump((unsigned char *)&icmp_recv, ret);
-		}
+	// close(receive_s);
 
-		// close(receive_s);
-		
 	return 0;
 }
