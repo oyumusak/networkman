c59b878c5beb250b86711e3026590b52f47ad78c
Oguzhan Ince <oguzhan.ince@protonmail.com>
2023-01-13 06:03:33 +0300

    mac

--- z_icmp_trace.c
+++ z_icmp_trace.c
@@ -20,31 +20,6 @@
 // sudo tcpdump -i any host 10.100.4.1 and 10.100.4.147
 // sudo tcpdump -i any host 192.168.1.117 and host 8.8.8.8
 
-// dumps raw memory in hex byte and printable split format
-void dump(const unsigned char *data_buffer, const unsigned int length)
-{
-	unsigned char byte;
-	unsigned int i, j;
-	for(i=0; i < length; i++) {
-		byte = data_buffer[i];
-		fprintf(stdout, "%02x ", data_buffer[i]);  // display byte in hex
-		if(((i%16)==15) || (i==length-1)) {
-			for(j=0; j < 15-(i%16); j++)
-				fprintf(stdout, "   ");
-			fprintf(stdout, "| ");
-			for(j=(i-(i%16)); j <= i; j++) {  // display printable bytes from line
-				byte = data_buffer[j];
-				if((byte > 31) && (byte < 127)) // outside printable char range
-					fprintf(stdout, "%c", byte);
-				else
-					fprintf(stdout, ".");
-			}
-			fprintf(stdout, "\n"); // end of the dump line (each line 16 bytes)
-		} // end if
-	} // end for
-}
-
-
 // generic checksum calculation algorithm
 unsigned short cksum(unsigned short *addr, int len)
 {
